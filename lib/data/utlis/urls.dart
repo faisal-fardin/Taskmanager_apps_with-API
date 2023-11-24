63863a1f061d6eae5bdee4ee4f4e5ed4ac6e8d22
niloy <faisalfardin77@gmail.com>
2023-11-24 16:54:12 +0600

    login api has been added

--- lib/data/utlis/urls.dart
+++ lib/data/utlis/urls.dart
@@ -3,4 +3,5 @@ class Urls{
   Urls._();
   static const String _baseUrl = 'https://task.teamrabbil.com/api/v1';
   static String registration = '$_baseUrl/registration';
+  static String logIn = '$_baseUrl/login';
 }
--- lib/ui/screen/logIn/login_screen.dart
+++ lib/ui/screen/logIn/login_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:taskmanager_apps_api/data/models/network_response.dart';
+import 'package:taskmanager_apps_api/data/services/network_caller.dart';
 import 'package:taskmanager_apps_api/ui/widgets/screen_background.dart';
 
+import '../../../data/utlis/urls.dart';
 import '../../../style_file/style.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -14,6 +17,39 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   bool isVisible = true;
 
+
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
+
+  bool _isLogINProgress = false;
+
+  Future<void> logIn() async{
+    _isLogINProgress = true;
+    if(mounted){
+      setState(() {});}
+    final NetworkResponse response = await NetworkCaller().postRequest(Urls.logIn, <String , dynamic>{
+      "email": _emailController.text.trim(),
+      "password": _passController.text,
+    });
+
+    _isLogINProgress = false;
+    if(mounted){setState(() {});}
+
+    if(response.isSuccess){
+      Get.offNamed('/ButtonNavBar');
+    }else{
+      if(mounted){
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text('Incorrect email or password')));
+      }
+
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,89 +58,111 @@ class _LoginScreenState extends State<LoginScreen> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(22.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 65,),
-                const Text(
-                  'Get Started With',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-                TextFormField(
-                  decoration: appInputStyle('Email'),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  obscureText: isVisible,
-                  decoration: appInputStyle(
-                    'Password',
-                    IconButton: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          isVisible = !isVisible;
-                        });
-                      },
-                      icon: Icon(
-                          isVisible ? Icons.visibility_off : Icons.visibility),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 65,),
+                  const Text(
+                    'Get Started With',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: buttonStyle(),
-                    onPressed: () {
-                      Get.offNamed('/ButtonNavBar');
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: appInputStyle('Email'),
+                    validator: (String? value) {
+                      if (value?.isEmpty ?? true) {
+                        return 'Enter Your Email';
+                      }
                     },
-                    child: const Icon(Icons.arrow_forward_ios,color: Colors.white,),
                   ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Center(
-                  child: TextButton(
-                    onPressed: () {
-                      Get.offNamed('/EmailVerification');
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    controller: _passController,
+                    obscureText: isVisible,
+                    validator: (String? value) {
+                      if ((value?.isEmpty ?? true) || value!.length <= 5) {
+                        return 'Enter a password more then 6 latter';
+                      }
                     },
-                    child: const Text(
-                      'Forgot Password?',
-                      style: TextStyle(fontSize: 15, color: Colors.grey),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Don't have account?",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 0.6,
+                    decoration: appInputStyle(
+                      'Password',
+                      IconButton: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isVisible = !isVisible;
+                          });
+                        },
+                        icon: Icon(
+                            isVisible ? Icons.visibility_off : Icons.visibility),
                       ),
                     ),
-                    TextButton(
-                      onPressed: () {
-                        Get.toNamed('/SignUpScreen');
-                      },
-                      child: const Text('Sign up'),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Visibility(
+                      visible:  _isLogINProgress == false,
+                      replacement: const Center(child: CircularProgressIndicator()),
+                      child: ElevatedButton(
+                        style: buttonStyle(),
+                        onPressed: () {
+                          if (!_formKey.currentState!.validate()) {
+                            return;
+                          }
+                          logIn();
+                        },
+                        child: const Icon(Icons.arrow_forward_ios,color: Colors.white,),
+                      ),
                     ),
-                  ],
-                )
-              ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Center(
+                    child: TextButton(
+                      onPressed: () {
+                        Get.offNamed('/EmailVerification');
+                      },
+                      child: const Text(
+                        'Forgot Password?',
+                        style: TextStyle(fontSize: 15, color: Colors.grey),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Don't have account?",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 0.6,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Get.toNamed('/SignUpScreen');
+                        },
+                        child: const Text('Sign up'),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
