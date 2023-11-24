3943cae71c8bdbf42a6a47ac895018fae7aefe22
niloy <faisalfardin77@gmail.com>
2023-11-24 15:08:16 +0600

    Network caller has been updated

--- lib/data/services/network_caller.dart
+++ lib/data/services/network_caller.dart
@@ -1,42 +1,45 @@
-
-
 import 'dart:convert';
 import 'dart:developer';
 
 import 'package:http/http.dart';
 import 'package:taskmanager_apps_api/data/models/network_response.dart';
 
-class NetworkCaller{
-
-  Future<NetworkResponse> getRequest(String url,) async{
+class NetworkCaller {
+  Future<NetworkResponse> getRequest(
+    String url,
+  ) async {
     try {
       Response response = await get(Uri.parse(url));
       if (response.statusCode == 200) {
-       return NetworkResponse(true, response.statusCode, jsonDecode(response.body));
+        return NetworkResponse(
+            true, response.statusCode, jsonDecode(response.body));
       } else {
         return NetworkResponse(false, response.statusCode, null);
       }
-    }catch (e){
-     log(e.toString());
+    } catch (e) {
+      log(e.toString());
     }
     return NetworkResponse(false, -1, null);
   }
 
-
-  Future<NetworkResponse> postRequest(String url, Map<String,dynamic> body) async{
+  Future<NetworkResponse> postRequest(
+      String url, Map<String, dynamic> body) async {
     try {
-      Response response = await post(Uri.parse(url) , body: jsonEncode(body));
+      Response response = await post(Uri.parse(url),
+          headers: {'Content-Type': 'application/json'},
+          body: jsonEncode(body),
+      );
+      log(response.statusCode.toString());
+      log(response.body);
       if (response.statusCode == 200) {
-       return NetworkResponse(true, response.statusCode, jsonDecode(response.body));
+        return NetworkResponse(
+            true, response.statusCode, jsonDecode(response.body));
       } else {
         return NetworkResponse(false, response.statusCode, null);
       }
-    }catch (e){
-     log(e.toString());
+    } catch (e) {
+      log(e.toString());
     }
     return NetworkResponse(false, -1, null);
   }
-
-
-
 }
--- lib/ui/screen/logIn/signup_screen.dart
+++ lib/ui/screen/logIn/signup_screen.dart
@@ -1,7 +1,11 @@
+import 'package:easy_loader/easy_loader.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:taskmanager_apps_api/data/models/network_response.dart';
+import 'package:taskmanager_apps_api/data/services/network_caller.dart';
 import 'package:taskmanager_apps_api/ui/widgets/screen_background.dart';
 
+import '../../../data/utlis/urls.dart';
 import '../../../style_file/style.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -14,109 +18,200 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   bool isVisible = true;
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
+
+  bool _isSignUpProgress = false;
+
+  Future<void> userSignUp() async {
+
+    _isSignUpProgress = true;
+    if(mounted){
+      setState(() {});
+    }
+    final NetworkResponse response =
+        await NetworkCaller().postRequest(Urls.registration, <String, dynamic>{
+      "email": _emailController.text.trim(),
+      "firstName": _firstNameController.text.trim(),
+      "lastName": _lastNameController.text.trim(),
+      "mobile": _phoneController.text.trim(),
+      "password": _passController.text,
+      "photo": ""
+    });
+    _isSignUpProgress = false;
+    if(mounted){
+      setState(() {});
+    }
+    if (response.isSuccess) {
+
+      _firstNameController.clear();
+      _lastNameController.clear();
+      _emailController.clear();
+      _phoneController.clear();
+      _passController.clear();
+
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Registration Success')));
+      }
+    } else {
+      if (mounted) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text('Registration Failed')));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ScreenBackground(
           child: SafeArea(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(22.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 65,),
-                    const Text(
-                      'Sign Up Now',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(22.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 65,
+                  ),
+                  const Text(
+                    'Sign Up Now',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    controller: _firstNameController,
+                    decoration: appInputStyle('First Name'),
+                    validator: (String? value) {
+                      if (value?.isEmpty ?? true) {
+                        return 'Enter Your First Name';
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    controller: _lastNameController,
+                    decoration: appInputStyle('Last name'),
+                    validator: (String? value) {
+                      if (value?.isEmpty ?? true) {
+                        return 'Enter Your Last Name';
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: appInputStyle('Email'),
+                    validator: (String? value) {
+                      if (value?.isEmpty ?? true) {
+                        return 'Enter Your Email';
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    controller: _phoneController,
+                    decoration: appInputStyle('Phone'),
+                    validator: (String? value) {
+                      if ((value?.isEmpty ?? true) || value!.length < 11) {
+                        return 'Enter Your Mobile Number';
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    controller: _passController,
+                    obscureText: isVisible,
+                    validator: (String? value) {
+                      if ((value?.isEmpty ?? true) || value!.length <= 5) {
+                        return 'Enter a password more then 6 latter';
+                      }
+                    },
+                    decoration: appInputStyle(
+                      'Password',
+                      IconButton: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isVisible = !isVisible;
+                          });
+                        },
+                        icon: Icon(isVisible
+                            ? Icons.visibility_off
+                            : Icons.visibility),
                       ),
                     ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      decoration: appInputStyle('First Name'),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      decoration: appInputStyle('Last name'),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      decoration: appInputStyle('Email'),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      decoration: appInputStyle('Phone'),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      obscureText: isVisible,
-                      decoration: appInputStyle(
-                        'Password',
-                        IconButton: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              isVisible = !isVisible;
-                            });
-                          },
-                          icon: Icon(
-                              isVisible ? Icons.visibility_off : Icons.visibility),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    SizedBox(
-                      width: double.infinity,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Visibility(
+                      visible: _isSignUpProgress == false,
+                      replacement: const Center(child: CircularProgressIndicator()),
                       child: ElevatedButton(
                         style: buttonStyle(),
                         onPressed: () {
-                          Get.offNamed('/LogIn');
+                          if (!_formKey.currentState!.validate()) {
+                            return;
+                          }
+                          userSignUp();
                         },
                         child: const Icon(Icons.arrow_forward_ios),
                       ),
                     ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          "Have an account?",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 0.6,
-                          ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Have an account?",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 0.6,
                         ),
-                        TextButton(
-                          onPressed: () {
-                            Get.toNamed('/SignUpScreen');
-                          },
-                          child: const Text('Sign up'),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Get.toNamed('/SignUpScreen');
+                        },
+                        child: const Text('Sign up'),
+                      ),
+                    ],
+                  )
+                ],
               ),
             ),
-          )),
+          ),
+        ),
+      )),
     );
   }
 }
-
