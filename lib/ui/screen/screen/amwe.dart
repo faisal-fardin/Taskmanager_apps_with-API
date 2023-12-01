fd55529667aa0aa4b296c3f4a369d63a2335146e
niloy <faisalfardin77@gmaul.com>
2023-12-01 10:06:20 +0600

    something

---
+++ lib/ui/screen/screen/amwe.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AmeraApps extends StatelessWidget {
+  const AmeraApps({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('amer apps'),
+      ),
+    );
+  }
+}
