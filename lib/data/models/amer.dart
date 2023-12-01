62f6c15dc44eb6ab6e16b6276307c57ff76cc9ad
niloy <faisalfardin77@gmaul.com>
2023-12-01 11:17:44 +0600

    add

---
+++ lib/data/models/amer.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class AmerApps extends StatelessWidget {
+  const AmerApps({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
